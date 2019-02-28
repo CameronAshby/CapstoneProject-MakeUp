a1fb1bd8027fcadc1c5b9f34d8bc5b11d2f35f43
Valerie <valleygirl021@gmail.com>
2019-02-28 16:25:00 -0700

    merge

--- src/app/service/login/login.service.ts
+++ src/app/service/login/login.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { User } from '../../models/user/user';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  signedIn: boolean;
+
+  constructor(public afAuth: AngularFireAuth, ) {}
+
+  signIn() {
+    this.signedIn = true;
+    return this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+
+  }
+  signOut(){
+    this.signedIn = false;
+  this.afAuth.auth.signOut();
+  }
+
+
 }
--- src/app/welcome-page/welcome-page.page.ts
+++ src/app/welcome-page/welcome-page.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {ApiService} from '../service/api/api.service';
-import {Observable} from 'rxjs';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {LoginService} from "../service/login/login.service";
+
 
 @Component({
   selector: 'app-welcome-page',
@@ -8,15 +8,45 @@ import {Observable} from 'rxjs';
   styleUrls: ['./welcome-page.page.scss'],
 })
 export class WelcomePagePage implements OnInit {
+  email: string;
+  password: string;
+  profilePic = '';
+  user;
 
-  product$: Observable<any>;
-
-  product: {};
-
-  constructor(private api: ApiService) {
-    this.product$ = this.api.initializeAPI();
+  constructor(private loginService: LoginService, private zone:NgZone) {
   }
 
   ngOnInit() {
   }
+
+  registerGoogle(){
+    let user = this.user;
+    this.loginService.signIn().then( data =>{
+      this.zone.run( ()=>{
+        user = {
+          name: data['user']['displayName'],
+          email: data['user']['email'],
+          cart: [],
+          profilePic: data['user']['photoURL']
+         }
+        });
+    });
+  }
+  registerEmail(){
+    let user = this.user;
+    this.loginService.signIn().then( data =>{
+        user = {
+          name: '',
+          email: '',
+          cart: [],
+          password: '',
+          profilePic: ''
+        }
+      });
+  }
+
+
+
 }
+
+
