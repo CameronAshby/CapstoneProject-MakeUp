f1cced396e3991aac4326efa821d07bd26fe72c9
Cameron Ashby <c00ashby@gmail.com>
2019-03-13 08:23:12 -0600

    worked on sign in page

--- src/app/welcome-page/welcome-page.page.ts
+++ src/app/welcome-page/welcome-page.page.ts
@@ -12,7 +12,7 @@ export class WelcomePagePage implements OnInit {
   name: string;
   password: string;
 
-  constructor(private loginService: LoginService, ) {
+  constructor(public loginService: LoginService, ) {
 
   }
 
@@ -24,7 +24,6 @@ export class WelcomePagePage implements OnInit {
   }
   updateName(name){
     this.name = name;
-    console.log("update Name" + this.name)
   }
 
 
