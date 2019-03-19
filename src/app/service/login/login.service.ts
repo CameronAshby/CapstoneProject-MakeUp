6e7097334f7eca541a760ea3c1c5f8028e23cb9a
Valerie <valleygirl021@gmail.com>
2019-03-18 21:38:06 -0600

    new user with pink

--- src/app/service/login/login.service.ts
+++ src/app/service/login/login.service.ts
@@ -1,89 +1,102 @@
 import {Injectable, OnInit} from '@angular/core';
 
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 import {map} from "rxjs/operators";
 import {
-  AngularFirestore,
-  AngularFirestoreCollection,
-  AngularFirestoreDocument,
-  DocumentChangeAction
+    AngularFirestore,
+    AngularFirestoreCollection,
+    AngularFirestoreDocument,
+    DocumentChangeAction
 } from "@angular/fire/firestore";
 import {User} from "../../model/User";
 import {AngularFireAuth} from "angularfire2/auth";
-import { auth } from "firebase/app";
+import {auth} from "firebase/app";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class LoginService implements OnInit{
+export class LoginService implements OnInit {
 
-  currentUser: User;
-  userName: string;
-
-
-  private userRef: AngularFirestoreDocument<User>;
-  private usersRef: AngularFirestoreCollection<User>;
+    currentUser: User;
+    userName: string;
 
 
-  constructor(private db: AngularFirestore, public afAuth: AngularFireAuth) {
-    this.usersRef = this.db.collection<User>(`users`);
-  }
-  ngOnInit(): void {
+    private userRef: AngularFirestoreDocument<User>;
+    private usersRef: AngularFirestoreCollection<User>;
 
-  }
 
-  googleSignIn() {
-    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
-        .then(data => {
-          this.currentUser = {
-            name: data.user.displayName,
-            email: data.user.email,
-            cart: [],
-          };
-          console.log(data);
-            this.saveUser(this.currentUser);
-        });
-    console.log("saved user to fb");
+    constructor(private db: AngularFirestore, public afAuth: AngularFireAuth) {
+        this.usersRef = this.db.collection<User>(`users`);
     }
 
-    emailSignIn(email, password) {
-    this.afAuth.auth.createUserWithEmailAndPassword(email, password)
-        .then( data => {
+    ngOnInit(): void {
+
+    }
+
+    googleSignIn() {
+        this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+            .then(data => {
+                this.currentUser = {
+                    name: data.user.displayName,
+                    email: data.user.email,
+                    cart: [],
+                };
+                console.log(data);
+                this.saveUser(this.currentUser);
+            });
+        console.log("saved user to fb");
+    }
+
+    newUser(email, password) {
+        this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+            .then(data => {
+                this.currentUser = {
+                    name: this.userName,
+                    email: data.user.email,
+                    cart: [],
+                    password: password
+                };
+                console.log("password" + password);
+                console.log("display name" + this.currentUser.name);
+                this.saveUser(this.currentUser);
+            });
+    }
+
+    emailSignIn(email, password){
+        this.afAuth.auth.signInWithEmailAndPassword(email, password)
+            .then(data => {
             this.currentUser = {
-                name: this.userName,
+                name: data.user.displayName,
                 email: data.user.email,
                 cart: [],
-                password: password
             };
-            console.log("password" + password);
-            console.log("display name" + this.currentUser.name);
-            this.saveUser(this.currentUser);
         });
+        console.log("email sign in");
     }
 
-  logout() {
-      this.afAuth.auth.signOut()
-          .then(data => {
-              this.currentUser = null;
-          });
-  }
+    logout() {
+        this.afAuth.auth.signOut()
+            .then(data => {
+                this.currentUser = null;
+            });
+    }
 
-  getUsersObservable(): Observable<User[]>{
-    return this.usersRef.snapshotChanges()
-        .pipe(
-            map((items: DocumentChangeAction<User>[]): User[] => {
-              return items.map((item: DocumentChangeAction<User>): User => {
-                return {
-                  name: item.payload.doc.data().name,
-                  email: item.payload.doc.data().email,
-                  cart: item.payload.doc.data().cart,
-                  password: item.payload.doc.data().password,
-                }as User;
-              });
-            })
-        );
+    getUsersObservable(): Observable<User[]> {
+        return this.usersRef.snapshotChanges()
+            .pipe(
+                map((items: DocumentChangeAction<User>[]): User[] => {
+                    return items.map((item: DocumentChangeAction<User>): User => {
+                        return {
+                            name: item.payload.doc.data().name,
+                            email: item.payload.doc.data().email,
+                            cart: item.payload.doc.data().cart,
+                            password: item.payload.doc.data().password,
+                        } as User;
+                    });
+                })
+            );
 
-  }
+    }
 
   saveUser(user: User) {
       this.usersRef.doc(this.afAuth.auth.currentUser.email).set(this.currentUser);
