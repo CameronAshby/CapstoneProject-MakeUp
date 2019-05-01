7e503eb63897249754a1c859b1105299f350eff6
Cameron Ashby <c00ashby@gmail.com>
2019-05-01 09:50:31 -0600

    fixed detail page add

--- src/app/shop/product-page/product-page.page.ts
+++ src/app/shop/product-page/product-page.page.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {LoginService} from '../../service/login/login.service';
 import {FirebaseService} from '../../service/firebase/firebase.service';
 import {Product} from '../../model/product';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-product-page',
@@ -17,7 +18,7 @@ export class ProductPagePage implements OnInit {
 
   similarProducts$: Observable<any>;
 
-  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService, public loginService: LoginService, public firebaseService:FirebaseService) { }
+  constructor(private toastController: ToastController, private router: Router, private route: ActivatedRoute, private api: ApiService, public loginService: LoginService, public firebaseService:FirebaseService) { }
 
   ngOnInit() {
     this.productId = this.route.snapshot.paramMap.get('productId');
@@ -36,6 +37,20 @@ export class ProductPagePage implements OnInit {
 
   showDetails(id: string) {
     this.router.navigate(['shop','product-page',id]);
+  }
+
+  addToCart(item) {
+    this.presentToast();
+    this.firebaseService.addToCart(item);
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Added to Cart',
+      duration: 800,
+      color: 'tertiary'
+    });
+    toast.present();
   }
 
 }
