3d84bc3af8616aac27833e026225f7075d26e8f0
Valerie <valleygirl021@gmail.com>
2019-05-08 18:37:34 -0600

    profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {LoginService} from '../service/login/login.service';
 import {Router} from '@angular/router';
 import {FirebaseService} from "../service/firebase/firebase.service";
-import {Product} from "../model/product";
+
 
 @Component({
   selector: 'app-profile',
@@ -11,29 +11,18 @@ import {Product} from "../model/product";
 })
 export class ProfilePage implements OnInit {
 
-  favorites: Product [];
-  seeFavs: boolean;
 
   constructor(public loginService: LoginService, private router: Router, public firebaseService: FirebaseService) { }
 
   ngOnInit() {
-  }
 
+  }
   ionViewDidEnter() {
     if(!this.loginService.currentUser) {
       this.router.navigate(['/welcome-page']);
     }
-
   }
 
-  toggleFavorites(){
-    if(this.seeFavs){
-      this.seeFavs = false;
-    }
-    else{
-      this.seeFavs = true;
-    }
-  }
 
 
 
